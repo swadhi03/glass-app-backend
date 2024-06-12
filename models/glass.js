bd48d85fad3641cf03c94a5181d05fe32ddac84e
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-12 21:03:40 +0530

    completed

---
+++ models/glass.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const schema=mongoose.Schema(
+    {
+        "title":{type:String,required:true},
+        "shape":{type:String,required:true},
+        "colour":{type:String,required:true},
+        "frame":{type:String,required:true},
+        "image":{type:String,require:true}
+    }
+)
+let glassmodel=mongoose.model("glasses",schema)
+module.exports={glassmodel}
